30a86d0fcdee2e30c37823e399752c40f58444c2
Andrey Oskin <a_oskin@wargaming.net>
2020-02-05 12:08:49 +0200

    Initial benchmarks

--- src/PushDirectBenchmark.jl
+++ src/PushDirectBenchmark.jl
@@ -1,5 +1,123 @@
 module PushDirectBenchmark
 
-greet() = print("Hello World!")
+using BenchmarkTools
+using StatsBase: median
+using DataFrames
+using VegaLite
+using Dates
 
+export plot_benchmarks
+
+function f1(n)
+    x = Vector{Int}(undef, n)
+    for i in 1:n
+        x[i] = i
+    end
+    x
+end
+
+function f2(n)
+    x = Int[]
+    for i in 1:n
+        push!(x, i)
+    end
+    x
+end
+
+function f3(n)
+    x = Int[]
+    sizehint!(x, n)
+    for i in 1:n
+        push!(x, i)
+    end
+    x
+end
+
+function f4(n)
+    x = Int[]
+    sizehint!(x, n)
+    for i in 1:n
+        x[i] = i
+    end
+    x
+end
+
+function f5(n)
+    x = Vector{Int}(undef, n)
+    for i in 1:n
+        push!(x, i)
+    end
+    x
+end
+
+function f6(n)
+    x = Vector{Int}(undef, n)
+    for i in 1:n
+        @inbounds x[i] = i
+    end
+    x
+end
+
+function plot_benchmarks(; dir = joinpath(@__DIR__, "..", "assets"),
+        maxn = 200_000,
+        name = Dates.format(now(), dateformat"yyyymmddTHHMMSS")*gethostname())
+
+    maxn = Int(floor(log2(maxn)))
+    paths = Dict{String, String}("comparison" => "push_direct_comparison.png",
+        "rel_max" => "push_direct_comparison_rel_max.png",
+        "rel_min" => "push_direct_comparison_rel_min.png")
+    for (k, v) in names
+        paths[k] = joinpath(dir, join([name, v], "_"))
+    end
+
+    df = DataFrame(n = Int[], val = Float64[], type = String[])
+    for n in 2 .^ collect(0:maxn)
+        d = @benchmark f1($n)
+        p = @benchmark f2($n)
+        h = @benchmark f3($n)
+        di = @benchmark f6($n)
+
+        push!(df, (n, median(d.times)/1000, "direct"))
+        push!(df, (n, median(p.times)/1000, "push"))
+        push!(df, (n, median(h.times)/1000, "push_hint"))
+        push!(df, (n, median(di.times)/1000, "direct_inbounds"))
+    end
+
+    df |> @vlplot(
+        :line,
+        background="white",
+        x={field=:n, scale={type="log"}, axis={gridWidth=0.5}},
+        y={field=:val, scale={type="log"}, axis={gridWidth=0.5, title="Median time, μs"}},
+        color=:type,
+        width=600,
+        height=600
+    ) |> x -> save(paths["comparison"], x)
+
+    df2 = by(df, :n) do d
+        DataFrame(val = d[:val], type = d[:type], val_max = maximum(d[:val]), val_min = minimum(d[:val]))
+    end
+
+    df2[:rel_max] = df2[:val] ./ df2[:val_max]
+    df2[:rel_min] = df2[:val] ./ df2[:val_min]
+
+    df2 |> @vlplot(
+        :line,
+        background="white",
+        x={field=:n, scale={type="log"}, axis={gridWidth=0.5}},
+        y={field=:rel_max, axis={gridWidth=0.5, title="Median time, μs"}},
+        color=:type,
+        width=600,
+        height=600
+    ) |> x -> save(paths["rel_max"], x)
+
+    df2 |> @vlplot(
+        :line,
+        background="white",
+        x={field=:n, scale={type="log"}, axis={gridWidth=0.5}},
+        y={field=:rel_min, axis={gridWidth=0.5, title="Median time, μs"}},
+        color=:type,
+        width=600,
+        height=600
+    ) |> x -> save(paths["rel_min"], x)
+end
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
+module PushDirectBenchmarkTest
 using PushDirectBenchmark
+using PushDirectBenchmark: f1, f2, f3, f4, f5, f6
 using Test
 
 @testset "PushDirectBenchmark.jl" begin
-    # Write your own tests here.
+    n = 1000
+    @test f1(n) == f2(n)
+    @test f1(n) == f3(n)
+    @test_throws BoundsError f4(n)
+    @test f1(n) != f5(n)
+    @test f1(n) == f6(n)
 end
+
+end # module
